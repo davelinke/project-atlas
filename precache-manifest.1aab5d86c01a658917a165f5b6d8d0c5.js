e2cf876d9bc9af3420ae441c316be760fecea253
David Linke <davidlinke@gmail.com>
2018-10-21 16:54:55 -0400

    updated to latest master build

---
+++ precache-manifest.1aab5d86c01a658917a165f5b6d8d0c5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "dd7371aad265ca99fac1",
+    "url": "/static/js/main.dd7371aa.chunk.js"
+  },
+  {
+    "revision": "47a911bf2d5b3c927938",
+    "url": "/static/js/1.47a911bf.chunk.js"
+  },
+  {
+    "revision": "dd7371aad265ca99fac1",
+    "url": "/static/css/main.1235ddcf.chunk.css"
+  },
+  {
+    "revision": "058462198289ef989dfc4fbaaf3f936d",
+    "url": "/index.html"
+  }
+];
