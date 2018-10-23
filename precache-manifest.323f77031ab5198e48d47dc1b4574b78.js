8d5c465e6d970d25e3fc475d3b8fc870c8efc9ca
David Linke <davidlinke@gmail.com>
2018-10-22 20:04:55 -0400

    Addidn latest compiled code.

---
+++ precache-manifest.323f77031ab5198e48d47dc1b4574b78.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "58c1ab07160fe167e364",
+    "url": "/static/js/main.58c1ab07.chunk.js"
+  },
+  {
+    "revision": "3fba576db83897f2fa99",
+    "url": "/static/js/1.3fba576d.chunk.js"
+  },
+  {
+    "revision": "58c1ab07160fe167e364",
+    "url": "/static/css/main.11789a5e.chunk.css"
+  },
+  {
+    "revision": "925c01722d889d453c1ef5d9bf9b3810",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1aab5d86c01a658917a165f5b6d8d0c5.js"
+  "/precache-manifest.323f77031ab5198e48d47dc1b4574b78.js"
 );
 
 workbox.clientsClaim();
